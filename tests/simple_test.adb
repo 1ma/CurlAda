9bc906f6a64c8f3d86befeb5042b77cd3e9cb5d3
Marcel Hernandez <e6990620@gmail.com>
2021-03-10 02:43:40 +0100

    initial commit

---
+++ tests/simple_test.adb
@@ -0,0 +1,17 @@
+with Ada.Text_IO;
+with CurlAda;
+with CurlAda.Easy;
+with Interfaces.C.Strings;
+
+procedure Simple_Test is
+   Handle : constant CurlAda.CURL := CurlAda.Easy.Init;
+   Code1, Code2 : CurlAda.CURLcode;
+begin
+   Code1 := CurlAda.Easy.Set_Opt (Handle, CurlAda.CURLOPT_URL, Interfaces.C.Strings.New_String ("https://www.google.com/"));
+   Code2 := CurlAda.Easy.Perform (Handle);
+   CurlAda.Easy.Cleanup (Handle);
+
+   Ada.Text_IO.New_Line;
+   Ada.Text_IO.Put_Line (Code1'Image);
+   Ada.Text_IO.Put_Line (Code2'Image);
+end Simple_Test;
