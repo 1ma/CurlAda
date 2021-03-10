4b5d433353ee01bf5d2f70a25c8f9c279d4b7adc
Marcel Hernandez <e6990620@gmail.com>
2021-03-10 22:57:23 +0100

    use another sample URL that yields less bulky HTML

--- tests/simple_test.adb
+++ tests/simple_test.adb
@@ -7,7 +7,7 @@ procedure Simple_Test is
    Handle : constant CurlAda.CURL := CurlAda.Easy.Init;
    Code1, Code2 : CurlAda.CURLcode;
 begin
-   Code1 := CurlAda.Easy.Set_Opt (Handle, CurlAda.CURLOPT_URL, Interfaces.C.Strings.New_String ("https://www.google.com/"));
+   Code1 := CurlAda.Easy.Set_Opt (Handle, CurlAda.CURLOPT_URL, Interfaces.C.Strings.New_String ("https://www.example.com/"));
    Code2 := CurlAda.Easy.Perform (Handle);
    CurlAda.Easy.Cleanup (Handle);
 
